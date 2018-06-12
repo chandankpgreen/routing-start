7c3e2814efe98bd38d8cb73563d7260c5e80dc22
chandank <chandank@meditab.com>
2018-06-12 13:22:48 +0530

    dummy authentication service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from "./auth-guard.service";
 
 
 const appRoutes: Routes = [
@@ -17,7 +18,10 @@ const appRoutes: Routes = [
         ]
     },
     {
-        path: "servers", component: ServersComponent, children: [
+        path: "servers",
+        // canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: ServersComponent, children: [
             { path: ":id", component: ServerComponent },
             { path: ":id/edit", component: EditServerComponent },
         ]
