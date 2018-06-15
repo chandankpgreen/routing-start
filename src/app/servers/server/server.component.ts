da67bda248becb59cb3cbf16ae430d5cb96a8019
chandank <chandank@meditab.com>
2018-06-15 18:29:22 +0530

    Resolver for async data

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -17,14 +17,19 @@ export class ServerComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    let serverId = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(serverId);
+    // let serverId = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(serverId);
 
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
+    // this.route.params.subscribe(
+    //   (params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //   }
+    // )
+    this.route.data.subscribe(
+      (data:Data)=>{
+        this.server = data['server'];
       }
-    )
+    );
   }
 
   onEdit() {
