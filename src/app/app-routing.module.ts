ad32de2573c3286f38aebd79b9a34a761b4315c3
chandank <chandank@meditab.com>
2018-06-15 19:22:25 +0530

    using hashed route so that servers dont have a problem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,12 +25,12 @@ const appRoutes: Routes = [
         // canActivate: [AuthGuard],
         canActivateChild: [AuthGuard],
         component: ServersComponent, children: [
-            { path: ":id", component: ServerComponent , resolve:{server: ServerResolver}},
+            { path: ":id", component: ServerComponent, resolve: { server: ServerResolver } },
             { path: ":id/edit", component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
         ]
     },
     // { path: 'not-found', component: PageNotFoundComponent },
-    { path: 'not-found', component: ErrorPageComponent, data:{message: 'Page not found!'} },
+    { path: 'not-found', component: ErrorPageComponent, data: { message: 'Page not found!' } },
     { path: 'something', redirectTo: '/not-found' },
     { path: '**', redirectTo: '/not-found' }
 ];
@@ -38,7 +38,10 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(
+            appRoutes
+            // ,{ useHash: true }
+        )
     ],
     exports: [
         RouterModule
